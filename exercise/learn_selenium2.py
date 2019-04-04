f374dacfb55af9eef0870ceca29c815f2c09a707
jwx <904653684@qq.com>
2019-04-04 13:03:34 +0800

    some exercise

--- exercise/learn_selenium2.py
+++ exercise/learn_selenium2.py
@@ -1,6 +1,6 @@
-import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 # from selenium.webdriver.support.ui import Select
 
 driver = webdriver.Chrome('D:/cs/python/chromedriver.exe')
@@ -20,7 +20,15 @@ def just_a_test():
     elem3 = driver.find_element_by_xpath("//button[@class='btn btn-primary btn-block loginbutton']")
     elem3.click()
 
-def tearDown(self):
-    driver.close()
+    elem4 = driver.find_element_by_xpath("//a[@href='/index.php/Home/Index/instrList/sid/4.html']")
+    elem4.click()
+
+    for i in range(1, 9):
+        driver.get("http://fxcszx.hitwh.edu.cn/index.php/Home/Index/instrList/sid/4/p/" + str(i) + "html")
+        equipment_list = driver.find_elements_by_css_selector('h4.media-heading')
+        for equipment in equipment_list:
+            print(equipment.text)
 
 just_a_test()
+time.sleep(3)
+driver.quit()
